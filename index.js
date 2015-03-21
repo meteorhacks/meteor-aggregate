a556bb288e023c74145ebbcc7fb0dc1aeff7ae68
Anthony <Anthony@AnthonysWeb.com>
2015-03-20 20:50:21 -0500

    Fix to work with Meteor 1.0.4
    There were some Mongo updates mentioned in the Meteor 1.0.4 announcement
    @ http://goo.gl/YwsbES I'm thinking were related.  Accessing internals
    is hackish and makes sense it broke?
    
    See GitHub issue @ https://github.com/meteorhacks/meteor-aggregate/issues/12

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 wrapAsync = (Meteor.wrapAsync)? Meteor.wrapAsync : Meteor._wrapAsync;
 Mongo.Collection.prototype.aggregate = function(pipelines) {
-  var coll = this._getCollection();
+  var coll = MongoInternals.defaultRemoteCollectionDriver().mongo.db.collection(this._name);
   return wrapAsync(coll.aggregate.bind(coll))(pipelines);
 }
