26d316fd8a5e431f7ef5d2a19eae86e6cc0f919c
Anthony <anthony123272@gmail.com>
2015-03-20 22:01:10 -0500

    Use exposed method not a hack, and make backwards compatable

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 wrapAsync = (Meteor.wrapAsync)? Meteor.wrapAsync : Meteor._wrapAsync;
 Mongo.Collection.prototype.aggregate = function(pipelines) {
-  var coll = MongoInternals.defaultRemoteCollectionDriver().mongo.db.collection(this._name);
+  var coll;
+  if (undefined !== this.rawCollection) {
+    // >= Meteor 1.0.4
+    coll = this.rawCollection();
+  } else {
+	// < Meteor 1.0.4
+    coll = this._getCollection();
+  }
   return wrapAsync(coll.aggregate.bind(coll))(pipelines);
 }
