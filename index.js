5028300b0c92f240f1e1c4873ca5d77c50b8e7c9
Valeriu Paloş <vpalos@users.noreply.github.com>
2017-10-27 16:38:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 wrapAsync = Meteor.wrapAsync || Meteor._wrapAsync;
 Mongo.Collection.prototype.aggregate = function(pipelines) {
   //         >= Meteor 1.0.4       < Meteor 1.0.4
-  var coll = this.rawCollection || this._getCollection;
+  var coll = (this.rawCollection || this._getCollection)();
   return wrapAsync(coll().aggregate.bind(coll))(pipelines);
 }
